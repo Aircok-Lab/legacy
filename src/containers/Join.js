057e379dc3facbb8eaac178931682a187179eafb
unknown <master@linkit.kr>
2019-01-23 13:36:36 +0900

    first commit

---
+++ src/containers/Join.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {Link} from 'react-router-dom';
+import IntlMessages from 'util/IntlMessages';
+import {
+  hideMessage,
+  showAuthLoader,
+  userSignUp
+} from 'actions/Auth';
+
+class Join extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.showMessage) {
+      setTimeout(() => {
+        this.props.hideMessage();
+      }, 3000);
+    }
+    if (this.props.authUser !== null) {
+      this.props.history.push('/');
+    }
+  }
+
+  render() {
+    const {
+      name,
+      email,
+      password
+    } = this.state;
+    const {showMessage, loader, alertMessage} = this.props;
+    return (
+      <div
+        className="app-login-container d-flex justify-content-center align-items-center animated slideInUpTiny animation-duration-3">
+        <div className="app-login-main-content">
+          <div className="app-login-content">
+            <div className="app-login-header">
+              <h1 className="text-center">회원가입</h1>
+            </div>
+
+            <div className="app-login-form">
+              <form method="post" action="/">
+                <TextField
+                  type="text"
+                  label="Name"
+                  onChange={(event) => this.setState({name: event.target.value})}
+                  fullWidth
+                  defaultValue={name}
+                  margin="normal"
+                  className="mt-0 mb-2"
+                />
+
+                <TextField
+                  type="email"
+                  onChange={(event) => this.setState({email: event.target.value})}
+                  label={<IntlMessages id="appModule.email"/>}
+                  fullWidth
+                  defaultValue={email}
+                  margin="normal"
+                  className="mt-0 mb-2"
+                />
+
+                <TextField
+                  type="password"
+                  onChange={(event) => this.setState({password: event.target.value})}
+                  label={<IntlMessages id="appModule.password"/>}
+                  fullWidth
+                  defaultValue={password}
+                  margin="normal"
+                  className="mt-0 mb-4"
+                />
+
+                <div className="mb-3 d-flex align-items-center justify-content-between">
+                  <Button className="btn-block" variant="raised" onClick={() => {
+                    this.props.showAuthLoader();
+                    this.props.userSignUp({email, password});
+                  }} color="primary">
+                    승인요청
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+
+        {
+          loader &&
+          <div className="loader-view">
+            <CircularProgress/>
+          </div>
+        }
+        {showMessage && NotificationManager.error(alertMessage)}
+        <NotificationContainer/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({auth}) => {
+  const {loader, alertMessage, showMessage, authUser} = auth;
+  return {loader, alertMessage, showMessage, authUser}
+};
+
+export default connect(mapStateToProps, {
+  userSignUp,
+  hideMessage,
+  showAuthLoader
+})(Join);
---
+++ src/containers/Login.js
@@ -0,0 +1,144 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import IntlMessages from "util/IntlMessages";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import {
+  hideMessage,
+  showAuthLoader,
+  userSignIn
+} from "actions/Auth";
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.showMessage) {
+      setTimeout(() => {
+        this.props.hideMessage();
+      }, 100);
+    }
+    if (this.props.authUser !== null) {
+      this.props.history.push('/');
+    }
+  }
+
+  handleChange = name => (event, checked) => {
+    //this.setState({[name]: checked});
+  };
+
+  render() {
+    const {
+      email,
+      password
+    } = this.state;
+    const {showMessage, loader, alertMessage} = this.props;
+    return (
+      <div
+        className="app-login-container d-flex justify-content-center align-items-center animated slideInUpTiny animation-duration-3">
+        <div className="app-login-main-content">
+
+          <div className="app-login-content">
+            <div className="app-login-header mb-4">
+              <h1 className="text-center">환경 IoT 센싱 데이터 모니터링 시스템</h1>
+            </div>
+
+            <div className="app-login-form">
+              <form>
+                <fieldset>
+                  <TextField
+                    label="아이디"
+                    fullWidth
+                    onChange={(event) => this.setState({email: event.target.value})}
+                    defaultValue={email}
+                    margin="normal"
+                    className="mt-1 my-sm-3"
+                  />
+                  <TextField
+                    type="password"
+                    label="비밀번호"
+                    fullWidth
+                    onChange={(event) => this.setState({password: event.target.value})}
+                    defaultValue={password}
+                    margin="normal"
+                    className="mt-1 my-sm-3"
+                  />
+
+                  <div className="mb-3 d-flex align-items-center justify-content-between">
+                    <FormGroup>
+                      <FormControlLabel
+                        control={
+                          <Checkbox color="primary"
+                                    checked={this.state.checkedB}
+                                    onChange={this.handleChange('checkedB')}
+                                    value="checkedB"
+                          />
+                        }
+                        label="로그인 상태 유지"
+                      />
+                    </FormGroup>
+                  </div>
+
+                  <div className="mb-3 d-flex align-items-center justify-content-between">
+                    <Button className="text-center btn-block" onClick={() => {
+                      this.props.showAuthLoader();
+                      this.props.userSignIn({email, password});
+                    }} variant="raised" color="primary">
+                      로그인
+                    </Button>
+                  </div>
+
+                  <div className="row mb-3 d-flex align-items-center justify-content-between">
+                    <div class="col text-center">
+                      <Link to="/join">
+                        회원가입
+                      </Link>
+                    </div>
+                    <div class="col text-center">
+                      <Link to="/forgot">
+                        아이디/비밀번호 찾기
+                      </Link>
+                    </div>
+                  </div>
+
+                </fieldset>
+              </form>
+            </div>
+          </div>
+
+        </div>
+        {
+          loader &&
+          <div className="loader-view">
+            <CircularProgress/>
+          </div>
+        }
+        {showMessage && console.log(alertMessage)}
+        {/*<NotificationContainer/>*/}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({auth}) => {
+  const {loader, alertMessage, showMessage, authUser} = auth;
+  return {loader, alertMessage, showMessage, authUser}
+};
+
+export default connect(mapStateToProps, {
+  userSignIn,
+  hideMessage,
+  showAuthLoader
+})(Login);
