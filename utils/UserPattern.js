f3a082d298e42dfe5e0d8e0cdbab470f4f5b3521
mira lee <980mira@gmail.com>
2019-03-14 13:48:32 +0900

    building/ position/ device list 없을 경우의 예외처리

--- utils/UserPattern.js
+++ utils/UserPattern.js
@@ -1,6 +1,7 @@
 var userPattern = {};
 
 userPattern.setBuildingListPattern = function(buildingList) {
+  if (!buildingList) return null;
   var insertStr = "," + "/";
   buildingList = buildingList.replace(/(\s*)/gi, "");
   if (!buildingList.endsWith(",")) buildingList = buildingList + ",";
@@ -10,6 +11,7 @@ userPattern.setBuildingListPattern = function(buildingList) {
 };
 
 userPattern.setPositionListPattern = function(positionList) {
+  if (!positionList) return null;
   var insertStr = "," + "/";
   positionList = positionList.replace(/(\s*)/gi, "");
   if (!positionList.endsWith(",")) positionList = positionList + ",";
@@ -19,6 +21,7 @@ userPattern.setPositionListPattern = function(positionList) {
 };
 
 userPattern.deletePattern = function(userInfo) {
+  if (!userInfo) return null;
   delete userInfo.Password;
   var insertStr = ",";
   var user = userInfo;
