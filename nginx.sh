a21d3b07086b303fbe1871808604066f41079fbe
mira lee <980mira@gmail.com>
2019-03-21 16:40:12 +0900

    delete 불필요한 파일 삭제

--- nginx.sh
+++
@@ -1,5 +0,0 @@
-sudo service nginx start
-cd /home/administrator/work/WebServer/
-pm2 start 'npm start' --name 'WebServer'
-cd /home/administrator/work/DeviceServer/SmartAircokDeviceServer/
-pm2 start npm -- start
