0caf5629d9b6a5fe128ff2b74b9c433ed5108c5a
Sangbum Woo <sbwoo87@gmail.com>
2019-03-24 10:36:43 +0900

    toaster border적용

--- src/util/toaster.js
+++ src/util/toaster.js
@@ -4,7 +4,7 @@ const toaster = (msg, duration, className) => {
     "style",
     "position:absolute;top:20%;margin:auto;left:50%;transform:translateX(-50%)"
   );
-  el.className = "text-white p-2 rounded-lg";
+  el.className = "text-white p-2 border border-dark";
   el.classList.add(className);
   el.innerHTML = msg;
   setTimeout(function() {
