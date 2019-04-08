9d819cec3fbc3d2c2107a9870418f6549b70e4d0
mira lee <980mira@gmail.com>
2019-04-08 09:54:56 +0900

    change database connectionLimit

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 // MySQL 데이터베이스 연결 설정
 var pool = mysql.createPool({
-  connectionLimit: "2",
+  connectionLimit: "10",
   host: "192.168.0.105",
   user: "root",
   password: "ethree0310",
