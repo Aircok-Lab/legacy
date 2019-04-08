45f23b1c12705f005ad1c0663cf22f44a2bde2e1
mira lee <980mira@gmail.com>
2019-04-08 11:33:41 +0900

    시간 정보 수정

--- routes/time.js
+++ routes/time.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  var now = Date();
+  var now = new Date()
+    .toISOString()
+    .replace(/[^0-9]/g, "")
+    .slice(0, -3);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end(now);
