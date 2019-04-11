48970d0ca914259026091877c1909be353eec7ce
mira lee <980mira@gmail.com>
2019-04-11 16:40:10 +0900

    proxy get 추가

--- routes/Proxy.js
+++ routes/Proxy.js
@@ -5,6 +5,27 @@ var router = express.Router();
 var Proxy = require("../models/Proxy");
 var global = require("../global");
 
+router.get("/", function(req, res, next) {
+  var promise = Proxy.get(req.query.url);
+  promise
+    .then(function(result) {
+      // res.send(result);
+      var result = { statusCode: null, message: null, data: null };
+      result.statusCode = OK;
+      result.message = "성공";
+      var json = JSON.parse(response);
+      result.data = json;
+      res.send(result);
+    })
+    .catch(function(err) {
+      console.dir(err);
+      var result = { statusCode: null, message: null, data: null };
+      result.statusCode = FAIL;
+      result.message = "실패";
+      res.send(result);
+    });
+});
+
 /* INSERT setting */
 router.post("/getToken", function(req, res, next) {
   console.log("/getToken 호출됨.");
