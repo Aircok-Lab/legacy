b0facb687b09294723773a88c4b62eceffe8126d
mira lee <980mira@gmail.com>
2019-04-17 16:07:09 +0900

    새로고침시 화면 안 나오는 문제 수정

--- src/app/routes/Detail/index.js
+++ src/app/routes/Detail/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import {
-  monitoringRecentDataRequest,
-  outdoorDataRequest
-} from "actions/RecentData";
+import { monitoringRecentDataRequest, outdoorDataRequest } from "actions/RecentData";
 import { showAuthLoader } from "actions/Auth";
 import { systemListRequest } from "actions/System";
 import SensorCard from "./card.js";
@@ -13,28 +10,28 @@ import AdviceInfo from "./adviceInfo.js";
 class DetailPage extends React.Component {
   constructor(props) {
     super(props);
-    var latitude = this.props.allRecentData[0].latitude;
-    var longitude = this.props.allRecentData[0].longitude;
+
     this.props.showAuthLoader();
     this.props.monitoringRecentDataRequest(this.props.match.params.deviceList);
     this.props.systemListRequest({ id: "1" });
-    this.props.outdoorDataRequest({ latitude, longitude });
+
     let device = this.props.match.params.deviceList.split(",");
     this.state = {
       index: 0,
-      deviceCnt: device.length,
-      deviceData: this.props.allRecentData[0]
+      deviceCnt: device.length
     };
     this.loadData = this.loadData.bind(this);
     this.scrollDevice = this.scrollDevice.bind(this);
+    if (this.props.allRecentData[0]) {
+      var latitude = this.props.allRecentData[this.state.index].latitude;
+      var longitude = this.props.allRecentData[this.state.index].longitude;
+      this.props.outdoorDataRequest({ latitude, longitude });
+    }
   }
 
   componentDidMount() {
     this.intervalLoadDataHandle = setInterval(this.loadData, 60000);
-    this.intervalScrollHandle = setInterval(
-      this.scrollDevice,
-      this.props.data.monitoringTime * 1000
-    );
+    this.intervalScrollHandle = setInterval(this.scrollDevice, this.props.data.monitoringTime * 1000);
   }
 
   componentWillUnmount() {
@@ -45,10 +42,13 @@ class DetailPage extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (this.props.data.monitoringTime !== nextProps.data.monitoringTime) {
       if (this.intervalScrollHandle) clearInterval(this.intervalScrollHandle);
-      this.intervalScrollHandle = setInterval(
-        this.scrollDevice,
-        nextProps.data.monitoringTime * 1000
-      );
+      this.intervalScrollHandle = setInterval(this.scrollDevice, nextProps.data.monitoringTime * 1000);
+    }
+
+    if (this.props.allRecentData !== nextProps.allRecentData) {
+      var latitude = nextProps.allRecentData[this.state.index].latitude;
+      var longitude = nextProps.allRecentData[this.state.index].longitude;
+      this.props.outdoorDataRequest({ latitude, longitude });
     }
   }
 
@@ -57,9 +57,14 @@ class DetailPage extends React.Component {
     if (this.state.deviceCnt - 1 !== this.state.index) i++;
     else i = 0;
     this.setState({
-      index: i,
-      deviceData: this.props.allRecentData[i]
+      index: i
     });
+
+    if (this.props.allRecentData[i]) {
+      var latitude = this.props.allRecentData[this.state.index].latitude;
+      var longitude = this.props.allRecentData[this.state.index].longitude;
+      this.props.outdoorDataRequest({ latitude, longitude });
+    }
   }
 
   loadData() {
@@ -67,78 +72,45 @@ class DetailPage extends React.Component {
   }
 
   render() {
-    const contact = this.state.deviceData;
+    const contact = this.props.allRecentData[this.state.index];
 
     return (
       <div className="h-100" style={{ overflow: "hidden" }}>
-        <div
-          className=""
-          style={{ background: "#e7ebee", width: "1920px", height: "1080px" }}
-        >
+        <div className="" style={{ background: "#e7ebee", width: "1920px", height: "1080px" }}>
           <div className="mt-3">
             <div className="card-deck mx-3 pb-3">
-              <SensorCard
-                title="통합공기질관리지수"
-                sensorData={contact.e3Score}
-                sensorIndex={contact.e3Index}
-                sensorType="totalindex"
-              />
-              <SensorCard
-                title="온도"
-                sensorData={contact.temperature}
-                sensorIndex={contact.temperatureIndex}
-                sensorType="temperature"
-              />
-              <SensorCard
-                title="습도"
-                sensorData={contact.humidity}
-                sensorIndex={contact.humidityIndex}
-                sensorType="humidity"
-              />
-              <SensorCard
-                title="미세먼지 (PM10)"
-                sensorData={contact.pm10}
-                sensorIndex={contact.pm10Index}
-                sensorType="pm10"
-              />
+              {contact ? (
+                <SensorCard title="통합공기질관리지수" sensorData={contact.e3Score} sensorIndex={contact.e3Index} sensorType="totalindex" />
+              ) : null}
+              {contact ? (
+                <SensorCard title="온도" sensorData={contact.temperature} sensorIndex={contact.temperatureIndex} sensorType="temperature" />
+              ) : null}
+              {contact ? <SensorCard title="습도" sensorData={contact.humidity} sensorIndex={contact.humidityIndex} sensorType="humidity" /> : null}
+              {contact ? <SensorCard title="미세먼지 (PM10)" sensorData={contact.pm10} sensorIndex={contact.pm10Index} sensorType="pm10" /> : null}
             </div>
             <div className="card-deck mx-3 pb-3">
-              <SensorCard
-                title="초미세먼지 (PM2.5)"
-                sensorData={contact.pm25}
-                sensorIndex={contact.pm25Index}
-                sensorType="pm25"
-              />
-              <SensorCard
-                title="이산화탄소 (CO2)"
-                sensorData={contact.co2}
-                sensorIndex={contact.co2Index}
-                sensorType="co2"
-              />
-              <SensorCard
-                title="포름알데히드 (HCHO)"
-                sensorData={contact.hcho}
-                sensorIndex={contact.hchoIndex}
-                sensorType="hcho"
-              />
-              <SensorCard
-                title="휘발성유기화합물 (VOCs)"
-                sensorData={contact.voc}
-                sensorIndex={contact.vocIndex}
-                sensorType="voc"
-              />
+              {contact ? <SensorCard title="초미세먼지 (PM2.5)" sensorData={contact.pm25} sensorIndex={contact.pm25Index} sensorType="pm25" /> : null}
+              {contact ? <SensorCard title="이산화탄소 (CO2)" sensorData={contact.co2} sensorIndex={contact.co2Index} sensorType="co2" /> : null}
+              {contact ? (
+                <SensorCard title="포름알데히드 (HCHO)" sensorData={contact.hcho} sensorIndex={contact.hchoIndex} sensorType="hcho" />
+              ) : null}
+              {contact ? (
+                <SensorCard title="휘발성유기화합물 (VOCs)" sensorData={contact.voc} sensorIndex={contact.vocIndex} sensorType="voc" />
+              ) : null}
             </div>
           </div>
           <div>
             <div className="card-deck mx-3 mb-3">
-              <DeviceInfo
-                time={contact.date}
-                buildingName={contact.buildingName}
-                positionName={contact.positionName}
-                deviceName={contact.deviceName}
-                serialNumber={contact.deviceSN}
-              />
-              <AdviceInfo deviceData={contact} />
+              {contact ? (
+                <DeviceInfo
+                  time={contact.date}
+                  buildingName={contact.buildingName}
+                  positionName={contact.positionName}
+                  deviceName={contact.deviceName}
+                  serialNumber={contact.deviceSN}
+                />
+              ) : null}
+              {contact ? <AdviceInfo deviceData={contact} /> : null}
             </div>
           </div>
         </div>
