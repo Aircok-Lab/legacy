ea51c4e92313e31e49bee88bfd165a748477fe7b
mira lee <980mira@gmail.com>
2019-04-17 16:06:39 +0900

    icon 안 나오는 문제 수정

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,125 +1,119 @@
-import React from 'react';
-import {Link, withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
-import AppBar from '@material-ui/core/AppBar';
-import Avatar from '@material-ui/core/Avatar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import {Dropdown, DropdownMenu, DropdownToggle} from 'reactstrap';
-import {
-    BELOW_THE_HEADER,
-    COLLAPSED_DRAWER,
-    FIXED_DRAWER,
-    HORIZONTAL_NAVIGATION,
-    INSIDE_THE_HEADER
-} from 'constants/ActionTypes';
-import SearchBox from 'components/SearchBox';
-import MailNotification from '../MailNotification/index';
-import AppNotification from '../AppNotification/index';
-import CardHeader from 'components/dashboard/Common/CardHeader/index';
-import {switchLanguage, toggleCollapsedNav} from 'actions/Setting';
-import IntlMessages from 'util/IntlMessages';
-import LanguageSwitcher from 'components/LanguageSwitcher/index';
-import Menu from 'components/TopNav/Menu';
-import UserInfoPopup from 'components/UserInfo/UserInfoPopup';
-import IndexHeader from '../Header/IndexHeader';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import AppBar from "@material-ui/core/AppBar";
+import Avatar from "@material-ui/core/Avatar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import { Dropdown, DropdownMenu, DropdownToggle } from "reactstrap";
+import { BELOW_THE_HEADER, COLLAPSED_DRAWER, FIXED_DRAWER, HORIZONTAL_NAVIGATION, INSIDE_THE_HEADER } from "constants/ActionTypes";
+import SearchBox from "components/SearchBox";
+import MailNotification from "../MailNotification/index";
+import AppNotification from "../AppNotification/index";
+import CardHeader from "components/dashboard/Common/CardHeader/index";
+import { switchLanguage, toggleCollapsedNav } from "actions/Setting";
+import IntlMessages from "util/IntlMessages";
+import LanguageSwitcher from "components/LanguageSwitcher/index";
+import Menu from "components/TopNav/Menu";
+import UserInfoPopup from "components/UserInfo/UserInfoPopup";
+import IndexHeader from "../Header/IndexHeader";
 
 class Header extends React.Component {
+  onAppNotificationSelect = () => {
+    this.setState({
+      appNotification: !this.state.appNotification
+    });
+  };
+  onMailNotificationSelect = () => {
+    this.setState({
+      mailNotification: !this.state.mailNotification
+    });
+  };
+  onLangSwitcherSelect = (event) => {
+    this.setState({
+      langSwitcher: !this.state.langSwitcher,
+      anchorEl: event.currentTarget
+    });
+  };
+  onSearchBoxSelect = () => {
+    this.setState({
+      searchBox: !this.state.searchBox
+    });
+  };
+  onUserInfoSelect = () => {
+    this.setState({
+      userInfo: !this.state.userInfo
+    });
+  };
+  handleRequestClose = () => {
+    this.setState({
+      langSwitcher: false,
+      userInfo: false,
+      mailNotification: false,
+      appNotification: false,
+      searchBox: false
+    });
+  };
 
-    onAppNotificationSelect = () => {
-        this.setState({
-            appNotification: !this.state.appNotification
-        })
+  constructor() {
+    super();
+    this.state = {
+      anchorEl: undefined,
+      searchBox: false,
+      searchText: "",
+      mailNotification: false,
+      userInfo: false,
+      langSwitcher: false,
+      appNotification: false
     };
-    onMailNotificationSelect = () => {
-        this.setState({
-            mailNotification: !this.state.mailNotification
-        })
-    };
-    onLangSwitcherSelect = (event) => {
-        this.setState({
-            langSwitcher: !this.state.langSwitcher, anchorEl: event.currentTarget
-        })
-    };
-    onSearchBoxSelect = () => {
-        this.setState({
-            searchBox: !this.state.searchBox
-        })
-    };
-    onUserInfoSelect = () => {
-        this.setState({
-            userInfo: !this.state.userInfo
-        })
-    };
-    handleRequestClose = () => {
-        this.setState({
-            langSwitcher: false,
-            userInfo: false,
-            mailNotification: false,
-            appNotification: false,
-            searchBox: false
-        });
-    };
+  }
 
+  onToggleCollapsedNav = (e) => {
+    const val = !this.props.navCollapsed;
+    this.props.toggleCollapsedNav(val);
+  };
 
-    constructor() {
-        super();
-        this.state = {
-            anchorEl: undefined,
-            searchBox: false,
-            searchText: '',
-            mailNotification: false,
-            userInfo: false,
-            langSwitcher: false,
-            appNotification: false,
-        }
-    }
+  updateSearchText(evt) {
+    this.setState({
+      searchText: evt.target.value
+    });
+  }
 
-    onToggleCollapsedNav = (e) => {
-        const val = !this.props.navCollapsed;
-        this.props.toggleCollapsedNav(val);
-    };
+  render() {
+    const { drawerType, locale, navigationStyle, horizontalNavPosition } = this.props;
+    const drawerStyle = drawerType.includes(FIXED_DRAWER) ? "d-block d-xl-none" : drawerType.includes(COLLAPSED_DRAWER) ? "d-block" : "d-none";
 
-    updateSearchText(evt) {
-        this.setState({
-            searchText: evt.target.value,
-        });
-    }
+    return (
+      <AppBar
+        className={`app-main-header ${
+          navigationStyle === HORIZONTAL_NAVIGATION && horizontalNavPosition === BELOW_THE_HEADER ? "app-main-header-top" : ""
+        }`}
+      >
+        <Toolbar className="app-toolbar" disableGutters={false}>
+          {navigationStyle === HORIZONTAL_NAVIGATION ? (
+            <div className="d-block d-md-none pointer mr-3" onClick={this.onToggleCollapsedNav}>
+              <span className="jr-menu-icon">
+                <span className="menu-icon" />
+              </span>
+            </div>
+          ) : (
+            <IconButton className={`jr-menu-icon mr-3 ${drawerStyle}`} aria-label="Menu" onClick={this.onToggleCollapsedNav}>
+              <span className="menu-icon" />
+            </IconButton>
+          )}
 
-    render() {
-        const {drawerType, locale, navigationStyle, horizontalNavPosition} = this.props;
-        const drawerStyle = drawerType.includes(FIXED_DRAWER) ? 'd-block d-xl-none' : drawerType.includes(COLLAPSED_DRAWER) ? 'd-block' : 'd-none';
+          <Link className="app-logo mr-2 d-none d-sm-block" to="/">
+            <img src="assets/icons/smart_aircok.png" alt="smart aircok" title="Smart Aircok" />
+          </Link>
+          <IndexHeader />
 
-        return (
-            <AppBar
-                className={`app-main-header ${(navigationStyle === HORIZONTAL_NAVIGATION && horizontalNavPosition === BELOW_THE_HEADER) ? 'app-main-header-top' : ''}`}>
-                <Toolbar className="app-toolbar" disableGutters={false}>
-                    {navigationStyle === HORIZONTAL_NAVIGATION ?
-                        <div className="d-block d-md-none pointer mr-3" onClick={this.onToggleCollapsedNav}>
-                            <span className="jr-menu-icon">
-                              <span className="menu-icon"/>
-                            </span>
-                        </div>
-                        :
-                        <IconButton className={`jr-menu-icon mr-3 ${drawerStyle}`} aria-label="Menu"
-                                    onClick={this.onToggleCollapsedNav}>
-                            <span className="menu-icon"/>
-                        </IconButton>
-                    }
-
-                    <Link className="app-logo mr-2 d-none d-sm-block" to="/">
-                        <img src="/assets/images/smart_aircok.png" alt="smart aircok" title="Smart Aircok"/>
-                    </Link>
-                    <IndexHeader />
-
-                    {/* <SearchBox styleName="d-none d-lg-block" placeholder=""
+          {/* <SearchBox styleName="d-none d-lg-block" placeholder=""
                                onChange={this.updateSearchText.bind(this)}
                                value={this.state.searchText}/>
                     {(navigationStyle === HORIZONTAL_NAVIGATION && horizontalNavPosition === INSIDE_THE_HEADER) &&
                     <Menu/>} */}
-                    
 
-                    {/* <ul className="header-notifications list-inline ml-auto">
+          {/* <ul className="header-notifications list-inline ml-auto">
                         <li className="d-inline-block d-lg-none list-inline-item">
                             <Dropdown
                                 className="quick-menu nav-searchbox"
@@ -237,17 +231,21 @@ class Header extends React.Component {
                                 </DropdownMenu>
                             </Dropdown>
                         </li>}
-                    </ul> */}                 
-                </Toolbar>
-            </AppBar>
-        );
-    }
-
+                    </ul> */}
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
-const mapStateToProps = ({settings}) => {
-    const {drawerType, locale, navigationStyle, horizontalNavPosition} = settings;
-    return {drawerType, locale, navigationStyle, horizontalNavPosition}
+const mapStateToProps = ({ settings }) => {
+  const { drawerType, locale, navigationStyle, horizontalNavPosition } = settings;
+  return { drawerType, locale, navigationStyle, horizontalNavPosition };
 };
 
-export default withRouter(connect(mapStateToProps, {toggleCollapsedNav, switchLanguage})(Header));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { toggleCollapsedNav, switchLanguage }
+  )(Header)
+);
