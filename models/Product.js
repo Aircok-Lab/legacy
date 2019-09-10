2c5646cbc14a32712630e19636b1bf91e08664a8
mira lee <980mira@gmail.com>
2019-09-10 16:22:58 +0900

    5분 데이터/ 1시간 데이터 API 추가

--- models/Product.js
+++ models/Product.js
@@ -74,6 +74,45 @@ var Product = {
       });
     });
   },
+  getProductBySerialNumber: function(serialNumber, callback) {
+    console.log("getProductBySerialNumber 호출됨");
+
+    pool.getConnection(function(err, conn) {
+      if (err) {
+        if (conn) {
+          conn.release(); // 반드시 해제해야 합니다.
+        }
+
+        callback(err, null);
+        return;
+      }
+      console.log("데이터베이스 연결 스레드 아이디 : " + conn.threadId);
+
+      // SQL문을 실행합니다.
+      var exec = conn.query(
+        "select Product.* from Device, Product where  Device.productID = Product.id and Device.serialNumber = ?",
+        serialNumber,
+        function(err, result) {
+          conn.release(); // 반드시 해제해야 합니다.
+          console.log("실행 대상 SQL : " + exec.sql);
+
+          if (err) {
+            console.log("SQL 실행 시 오류 발생함");
+            console.dir(err);
+
+            callback(err, null);
+            return;
+          }
+          var string = JSON.stringify(result);
+          var json = JSON.parse(string);
+          console.log(">> json: ", json);
+          var productInfo = json[0];
+
+          callback(null, productInfo);
+        }
+      );
+    });
+  },
   addProduct: function(
     name,
     version,
--- routes/Report.js
+++ routes/Report.js
@@ -2,6 +2,7 @@ import { OK, FAIL } from "../public/javascripts/defined";
 var express = require("express");
 var router = express.Router();
 var report = require("../models/Report");
+var Product = require("../models/Product");
 
 router.post("/getAVGe3ScoreByDate", function(req, res, next) {
   console.log("/getAVGe3ScoreByDate 호출됨.");
@@ -11,33 +12,45 @@ router.post("/getAVGe3ScoreByDate", function(req, res, next) {
   var paramEndDate = req.body.endDate || req.query.endDate;
   var result = { statusCode: null, message: null, data: null };
 
-  console.log("요청 파라미터 : " + paramSerialNumber + "," + paramStartDate + "," + paramEndDate);
+  console.log(
+    "요청 파라미터 : " +
+      paramSerialNumber +
+      "," +
+      paramStartDate +
+      "," +
+      paramEndDate
+  );
 
-  report.getAVGe3ScoreByDate(paramSerialNumber, paramStartDate, paramEndDate, function(err, addedAverage) {
-    // 동일한 id로 추가할 때 오류 발생 - 클라이언트 오류 전송
+  report.getAVGe3ScoreByDate(
+    paramSerialNumber,
+    paramStartDate,
+    paramEndDate,
+    function(err, addedAverage) {
+      // 동일한 id로 추가할 때 오류 발생 - 클라이언트 오류 전송
 
-    if (err) {
-      console.error("스코어 추가 중 오류 발생 :" + err.stack);
-      result.statusCode = FAIL;
-      result.message = "오류 발생";
-      res.send(result);
-      return;
+      if (err) {
+        console.error("스코어 추가 중 오류 발생 :" + err.stack);
+        result.statusCode = FAIL;
+        result.message = "오류 발생";
+        res.send(result);
+        return;
+      }
+
+      //결과 객체 있으면 성공 응답 전송
+      if (addedAverage) {
+        console.dir(addedAverage);
+        console.log("추가된 레코드의 아이디 : " + addedAverage);
+        result.statusCode = OK;
+        result.message = "성공";
+        result.data = addedAverage;
+        res.send(result);
+      } else {
+        result.statusCode = FAIL;
+        result.message = "실패";
+        res.send(result);
+      }
     }
-
-    //결과 객체 있으면 성공 응답 전송
-    if (addedAverage) {
-      console.dir(addedAverage);
-      console.log("추가된 레코드의 아이디 : " + addedAverage);
-      result.statusCode = OK;
-      result.message = "성공";
-      result.data = addedAverage;
-      res.send(result);
-    } else {
-      result.statusCode = FAIL;
-      result.message = "실패";
-      res.send(result);
-    }
-  });
+  );
 });
 
 router.post("/getCountE3Index", function(req, res, next) {
@@ -48,33 +61,48 @@ router.post("/getCountE3Index", function(req, res, next) {
   var paramE3Index = req.body.e3Index || req.query.e3Index;
   var result = { statusCode: null, message: null, data: null };
 
-  console.log("요청 파라미터 : " + paramSerialNumber + "," + paramStartDate + "," + paramEndDate + "," + paramE3Index);
+  console.log(
+    "요청 파라미터 : " +
+      paramSerialNumber +
+      "," +
+      paramStartDate +
+      "," +
+      paramEndDate +
+      "," +
+      paramE3Index
+  );
 
-  report.getCountE3Index(paramSerialNumber, paramStartDate, paramEndDate, paramE3Index, function(err, countedIndex) {
-    // 동일한 id로 추가할 때 오류 발생 - 클라이언트 오류 전송
+  report.getCountE3Index(
+    paramSerialNumber,
+    paramStartDate,
+    paramEndDate,
+    paramE3Index,
+    function(err, countedIndex) {
+      // 동일한 id로 추가할 때 오류 발생 - 클라이언트 오류 전송
 
-    if (err) {
-      console.error("인덱스 카운트 중 오류 발생 :" + err.stack);
-      result.statusCode = FAIL;
-      result.message = "오류 발생";
-      res.send(result);
-      return;
+      if (err) {
+        console.error("인덱스 카운트 중 오류 발생 :" + err.stack);
+        result.statusCode = FAIL;
+        result.message = "오류 발생";
+        res.send(result);
+        return;
+      }
+
+      //결과 객체 있으면 성공 응답 전송
+      if (countedIndex) {
+        console.dir(countedIndex);
+        console.log("추가된 레코드의 아이디 : " + countedIndex);
+        result.statusCode = OK;
+        result.message = "성공";
+        result.data = countedIndex;
+        res.send(result);
+      } else {
+        result.statusCode = FAIL;
+        result.message = "실패";
+        res.send(result);
+      }
     }
-
-    //결과 객체 있으면 성공 응답 전송
-    if (countedIndex) {
-      console.dir(countedIndex);
-      console.log("추가된 레코드의 아이디 : " + countedIndex);
-      result.statusCode = OK;
-      result.message = "성공";
-      result.data = countedIndex;
-      res.send(result);
-    } else {
-      result.statusCode = FAIL;
-      result.message = "실패";
-      res.send(result);
-    }
-  });
+  );
 });
 
 router.post("/getChartDataByDate", function(req, res, next) {
@@ -85,28 +113,158 @@ router.post("/getChartDataByDate", function(req, res, next) {
   var paramEndDate = req.body.endDate || req.query.endDate;
   var result = { statusCode: null, message: null, data: null };
 
-  console.log("요청 파라미터 : " + paramSerialNumber + "," + paramStartDate + "," + paramEndDate);
+  console.log(
+    "요청 파라미터 : " +
+      paramSerialNumber +
+      "," +
+      paramStartDate +
+      "," +
+      paramEndDate
+  );
 
-  report.getChartDataByDate(paramSerialNumber, paramStartDate, paramEndDate, function(err, chartData) {
-    // 동일한 id로 추가할 때 오류 발생 - 클라이언트 오류 전송
+  report.getChartDataByDate(
+    paramSerialNumber,
+    paramStartDate,
+    paramEndDate,
+    function(err, chartData) {
+      // 동일한 id로 추가할 때 오류 발생 - 클라이언트 오류 전송
 
+      if (err) {
+        console.error("데이터 로드중 오류 발생 :" + err.stack);
+        result.statusCode = FAIL;
+        result.message = "오류 발생";
+        res.send(result);
+        return;
+      }
+
+      //결과 객체 있으면 성공 응답 전송
+      if (chartData) {
+        result.statusCode = OK;
+        result.message = "성공";
+        result.data = chartData;
+        res.send(result);
+      } else {
+        result.statusCode = FAIL;
+        result.message = "실패";
+        res.send(result);
+      }
+    }
+  );
+});
+
+router.post("/get5minDataByDate", function(req, res, next) {
+  console.log("/get5minDataByDate 호출됨.");
+
+  var paramSerialNumber = req.body.serialNumber || req.query.serialNumber;
+  var paramStartDate = req.body.startDate || req.query.startDate;
+  var paramEndDate = req.body.endDate || req.query.endDate;
+  var result = { statusCode: null, message: null, data: null };
+
+  console.log(
+    "요청 파라미터 : " +
+      paramSerialNumber +
+      "," +
+      paramStartDate +
+      "," +
+      paramEndDate
+  );
+
+  Product.getProductBySerialNumber(paramSerialNumber, function(
+    err,
+    productInfo
+  ) {
     if (err) {
-      console.error("데이터 로드중 오류 발생 :" + err.stack);
-      result.statusCode = FAIL;
-      result.message = "오류 발생";
-      res.send(result);
+      console.error("Data table 추가 중 오류 발생 :" + err.stack);
       return;
     }
-
-    //결과 객체 있으면 성공 응답 전송
-    if (chartData) {
-      result.statusCode = OK;
-      result.message = "성공";
-      result.data = chartData;
-      res.send(result);
+    if (productInfo) {
+      report.get5minDataByDate(
+        productInfo,
+        paramSerialNumber,
+        paramStartDate,
+        paramEndDate,
+        function(err, data) {
+          if (err) {
+            console.error("Data table 추가 중 오류 발생 :" + err.stack);
+            return;
+          }
+          if (data) {
+            console.log("Data 성공");
+            result.statusCode = OK;
+            result.message = "성공";
+            result.data = data;
+            res.send(result);
+          } else {
+            console.log("data 정보가 없음");
+            result.statusCode = FAIL;
+            result.message = "data 정보가 없음";
+            res.send(result);
+          }
+        }
+      );
     } else {
+      console.log("Product 정보가 없음");
       result.statusCode = FAIL;
-      result.message = "실패";
+      result.message = "Product 정보가 없음";
+      res.send(result);
+    }
+  });
+});
+
+router.post("/getHourDataByDate", function(req, res, next) {
+  console.log("/getHourDataByDate 호출됨.");
+
+  var paramSerialNumber = req.body.serialNumber || req.query.serialNumber;
+  var paramStartDate = req.body.startDate || req.query.startDate;
+  var paramEndDate = req.body.endDate || req.query.endDate;
+  var result = { statusCode: null, message: null, data: null };
+
+  console.log(
+    "요청 파라미터 : " +
+      paramSerialNumber +
+      "," +
+      paramStartDate +
+      "," +
+      paramEndDate
+  );
+
+  Product.getProductBySerialNumber(paramSerialNumber, function(
+    err,
+    productInfo
+  ) {
+    if (err) {
+      console.error("Data table 추가 중 오류 발생 :" + err.stack);
+      return;
+    }
+    if (productInfo) {
+      report.getHourDataByDate(
+        productInfo,
+        paramSerialNumber,
+        paramStartDate,
+        paramEndDate,
+        function(err, data) {
+          if (err) {
+            console.error("Data table 추가 중 오류 발생 :" + err.stack);
+            return;
+          }
+          if (data) {
+            console.log("Data 성공");
+            result.statusCode = OK;
+            result.message = "성공";
+            result.data = data;
+            res.send(result);
+          } else {
+            console.log("data 정보가 없음");
+            result.statusCode = FAIL;
+            result.message = "data 정보가 없음";
+            res.send(result);
+          }
+        }
+      );
+    } else {
+      console.log("Product 정보가 없음");
+      result.statusCode = FAIL;
+      result.message = "Product 정보가 없음";
       res.send(result);
     }
   });
