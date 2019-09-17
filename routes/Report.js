c1e1f5e55b0d64d6a46da267d576ec51c9833f06
mira lee <980mira@gmail.com>
2019-09-17 17:51:26 +0900

    add comment api

--- routes/Report.js
+++ routes/Report.js
@@ -270,4 +270,343 @@ router.post("/getHourDataByDate", function(req, res, next) {
   });
 });
 
+router.post("/getTotalComment", function(req, res, next) {
+  console.log("/getTotalComment 호출됨.");
+
+  var paramDeviceData = req.body.deviceData || req.query.deviceData || null;
+  var result = { statusCode: null, message: null, data: null };
+  if (!paramDeviceData) {
+    console.log("deviceData 정보가 없음");
+    result.statusCode = FAIL;
+    result.message = "deviceData 정보가 없음";
+    res.send(result);
+    return;
+  }
+
+  console.log("요청 파라미터 : " + paramDeviceData);
+
+  var device = {
+    e3Index: paramDeviceData.e3Index,
+    data: [
+      {
+        name: "온도",
+        index: paramDeviceData.temperatureIndex,
+        advice: "냉/난방기를 "
+      },
+      {
+        name: "습도",
+        index: paramDeviceData.humidityIndex,
+        advice: "가습/제습 장치를 "
+      },
+      {
+        name: "PM25",
+        index: paramDeviceData.pm25Index,
+        advice: "공기청정기를 "
+      },
+      {
+        name: "PM10",
+        index: paramDeviceData.pm10Index,
+        advice: "공기청정기를 "
+      },
+      {
+        name: "CO2",
+        index: paramDeviceData.co2Index,
+        advice: "환기장치를 "
+      },
+      {
+        name: "HCHO",
+        index: paramDeviceData.hchoIndex,
+        advice: "환기장치를 "
+      },
+      {
+        name: "VOC",
+        index: paramDeviceData.vocIndex,
+        advice: "환기장치를 "
+      } //,
+      // {
+      //   name: "noise",
+      //   index: paramDeviceData.noiseIndex
+      // }
+    ]
+  };
+
+  device.data.sort(function(a, b) {
+    return a.index < b.index ? 1 : a.index > b.index ? -1 : 0;
+  });
+
+  let adviceStr1 = "",
+    adviceStr2 = "",
+    adviceStr3 = "",
+    adviceStr4 = "";
+  let cnt = 0;
+
+  if (device.e3Index === 1 || device.e3Index === 2) {
+    if (device.e3Index === 1)
+      adviceStr1 += "실내공간 공기질이 쾌적한 상태가 유지되고 있습니다.";
+    else if (device.e3Index === 2)
+      adviceStr1 += "실내공간 공기질이 전반적으로 쾌적한 상태입니다.";
+
+    device.data.map(sensor => {
+      if (sensor.index > 3) {
+        adviceStr2 += sensor.name + ",";
+        cnt++;
+        if (cnt == 1) {
+          adviceStr3 =
+            sensor.name +
+            " 관리를 위하여 " +
+            sensor.advice +
+            "작동시켜 주세요.";
+        }
+        if (cnt == 2) {
+          adviceStr4 =
+            sensor.name +
+            " 관리를 위하여 " +
+            sensor.advice +
+            "작동시켜 주세요.";
+        }
+      }
+    });
+    let temp = adviceStr2.slice(0, -1);
+    if (cnt > 0) adviceStr2 = temp + " 관리가 필요합니다.";
+  } else {
+    device.data.map(sensor => {
+      if (sensor.index > 3) {
+        adviceStr1 += sensor.name + ",";
+        cnt++;
+      }
+    });
+    adviceStr1 = adviceStr1.slice(0, -1) + "가 좋지 않습니다.";
+    if (cnt > 2) adviceStr2 = "특히 ";
+    adviceStr2 +=
+      device.data[0].name + ", " + device.data[1].name + " 관리가 필요합니다.";
+    adviceStr3 =
+      device.data[0].name +
+      " 관리를 위하여 " +
+      device.data[0].advice +
+      "작동시켜 주세요.";
+    adviceStr4 =
+      device.data[1].name +
+      " 관리를 위하여 " +
+      device.data[1].advice +
+      "작동시켜 주세요.";
+  }
+  result.statusCode = OK;
+  result.message = "성공";
+  result.data = adviceStr1 + adviceStr2 + adviceStr3 + adviceStr4;
+  res.send(result);
+});
+
+router.post("/getSensorComment", function(req, res, next) {
+  console.log("/getSensorComment 호출됨.");
+
+  var paramSensorType = req.body.sensorType || req.query.sensorType || null;
+  var paramIndex = req.body.index || req.query.index || null;
+  var result = { statusCode: null, message: null, data: null };
+
+  console.log("요청 파라미터 : " + paramSensorType + paramIndex);
+
+  if (!paramSensorType || !paramIndex) {
+    console.log("sensorData 정보가 없음");
+    result.statusCode = FAIL;
+    result.message = "sensorData 정보가 없음";
+    res.send(result);
+    return;
+  }
+
+  var data = {};
+  if (paramSensorType === "temperature") {
+    data = {
+      name: "온도",
+      index: paramIndex,
+      advice: "냉/난방기를 "
+    };
+  } else if (paramSensorType === "humidity") {
+    data = {
+      name: "습도",
+      index: paramIndex,
+      advice: "가습/제습 장치를 "
+    };
+  } else if (paramSensorType === "pm25") {
+    data = {
+      name: "PM25",
+      index: paramIndex,
+      advice: "공기청정기를 "
+    };
+  } else if (paramSensorType === "pm10") {
+    data = {
+      name: "PM10",
+      index: paramIndex,
+      advice: "공기청정기를 "
+    };
+  } else if (paramSensorType === "co2") {
+    data = {
+      name: "CO2",
+      index: paramIndex,
+      advice: "환기장치를 "
+    };
+  } else if (paramSensorType === "hcho") {
+    data = {
+      name: "HCHO",
+      index: paramIndex,
+      advice: "환기장치를 "
+    };
+  } else if (paramSensorType === "voc") {
+    data = {
+      name: "VOC",
+      index: paramIndex,
+      advice: "환기장치를 "
+    };
+  } else if (paramSensorType === "co") {
+    data = {
+      name: "CO",
+      index: paramIndex,
+      advice: "환기장치를 "
+    };
+  } else if (paramSensorType === "noise") {
+    data = {
+      name: "소음",
+      index: paramIndex,
+      advice: "볼륨을 "
+    };
+  }
+
+  let adviceStr = "";
+
+  if (data.index == 1) {
+    adviceStr = data.name + " 상태가 좋게 유지되고 있습니다.";
+  } else if (data.index == 2) {
+    adviceStr = data.name + " 상태가 보통으로 유지되고 있습니다.";
+  } else if (data.index == 3) {
+    adviceStr =
+      data.name +
+      " 상태가 약간나쁨으로 유지되고 있습니다." +
+      data.name +
+      " 관리가 필요합니다.";
+  } else if (data.index == 4) {
+    adviceStr =
+      data.name +
+      " 상태가 나쁨으로 유지되고 있습니다." +
+      data.name +
+      " 관리가 필요합니다. " +
+      data.name +
+      " 관리를 위하여 " +
+      data.advice +
+      "작동시켜 주세요.";
+  } else if (data.index == 5) {
+    adviceStr =
+      data.name +
+      " 상태가 매우나쁨으로 유지되고 있습니다." +
+      data.name +
+      " 관리가 필요합니다. " +
+      data.name +
+      " 관리를 위하여 " +
+      data.advice +
+      "작동시켜 주세요.";
+  } else if (data.index == 6) {
+    adviceStr =
+      data.name +
+      " 상태가 최악으로 유지되고 있습니다." +
+      data.name +
+      " 관리가 필요합니다. " +
+      data.name +
+      " 관리를 위하여 " +
+      data.advice +
+      "작동시켜 주세요.";
+  }
+
+  result.statusCode = OK;
+  result.message = "성공";
+  result.data = adviceStr;
+  res.send(result);
+});
+
+router.post("/getSensorAlert", function(req, res, next) {
+  console.log("/getSensorAlert 호출됨.");
+
+  var paramSensorType = req.body.sensorType || req.query.sensorType || null;
+  var paramAlarm = req.body.alarm || req.query.alarm || null;
+  var result = { statusCode: null, message: null, data: null };
+
+  console.log("요청 파라미터 : " + paramSensorType + paramAlarm);
+
+  if (!paramSensorType) {
+    console.log("sensorData 정보가 없음");
+    result.statusCode = FAIL;
+    result.message = "sensorData 정보가 없음";
+    res.send(result);
+    return;
+  }
+
+  var data = {};
+  if (paramSensorType === "temperature") {
+    data = {
+      name: "온도",
+      alarm: paramAlarm,
+      advice: "냉/난방기를 "
+    };
+  } else if (paramSensorType === "humidity") {
+    data = {
+      name: "습도",
+      alarm: paramAlarm,
+      advice: "가습/제습 장치를 "
+    };
+  } else if (paramSensorType === "pm25") {
+    data = {
+      name: "PM25",
+      alarm: paramAlarm,
+      advice: "공기청정기를 "
+    };
+  } else if (paramSensorType === "pm10") {
+    data = {
+      name: "PM10",
+      alarm: paramAlarm,
+      advice: "공기청정기를 "
+    };
+  } else if (paramSensorType === "co2") {
+    data = {
+      name: "CO2",
+      alarm: paramAlarm,
+      advice: "환기장치를 "
+    };
+  } else if (paramSensorType === "hcho") {
+    data = {
+      name: "HCHO",
+      alarm: paramAlarm,
+      advice: "환기장치를 "
+    };
+  } else if (paramSensorType === "voc") {
+    data = {
+      name: "VOC",
+      alarm: paramAlarm,
+      advice: "환기장치를 "
+    };
+  } else if (paramSensorType === "co") {
+    data = {
+      name: "CO",
+      alarm: paramAlarm,
+      advice: "환기장치를 "
+    };
+  } else if (paramSensorType === "noise") {
+    data = {
+      name: "소음",
+      alarm: paramAlarm,
+      advice: "볼륨을 "
+    };
+  }
+
+  let adviceStr = "";
+
+  if (data.alarm == 1) {
+    adviceStr =
+      data.name + " 수치가 높습니다. " + data.advice + "작동시켜 주세요.";
+  } else if (data.alarm == 0) {
+    adviceStr = data.name + " 수치가 낮아지고 있어요.";
+  }
+
+  result.statusCode = OK;
+  result.message = "성공";
+  result.data = adviceStr;
+  res.send(result);
+});
+
 module.exports = router;
