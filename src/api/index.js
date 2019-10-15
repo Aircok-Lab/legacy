096adeef2289029bcb6b44eb7f705be08d5bc0d7
whdguq3080 <01099473080@naver.com>
2019-10-15 10:54:41 +0900

    2019.10.15 10:54

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: `http://localhost:3000/`,
-  baseURL: `http://115.178.65.141:13701/`,
+  baseURL: `http://115.178.66.7:13701/`,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
